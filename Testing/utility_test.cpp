90cf1d405a64ef19fdefa60ddcf95792114b5fcd
Kyle Beyer <beykyle@umich.edu>
2018-05-12 23:21:19 -0400

    tests for linearizeIndices

--- Testing/utility_test.cpp
+++ Testing/utility_test.cpp
@@ -50,11 +50,48 @@ TEST_CASE( "Utility", "[utility]" ) {
   SECTION ( " return dot product of a vector " ) {
     REQUIRE( Utility::vecDot( doubleVec, doubleVec2 ) == 70 );
   }
+}
 
 /* ****************************************************************************************************** * 
  * Miscellaneous                      
  *
  * ****************************************************************************************************** */ 
+
+
+TEST_CASE( "linearizeIndices" , "[Utility]") {
+  
+  // trivial case 1d |-> 1d array
+  vector<int> indices  {3};
+  vector<int> binSizes {10};
+
+  
+  SECTION ( " 1d |-> 1d map " ) {
+    REQUIRE( Utility::linearizeIndices( indices , binSizes) == 3 );
+  }
+  
+  // case 2d |-> 1d array
+  vector<int> indices2d  {3,1};
+  vector<int> binSizes2d {5,3};
+  
+  SECTION ( " 2d |-> 1d map " ) {
+    REQUIRE( Utility::linearizeIndices( indices2d , binSizes2d ) == 8 );
+  }
+  
+  // case 3d |-> 1d array
+  vector<int> indices3d  {3,1,1};
+  vector<int> binSizes3d {5,3,2};
+  
+  SECTION ( " 3d |-> 1d map " ) {
+    REQUIRE( Utility::linearizeIndices( indices3d , binSizes3d ) == 23 );
+  }
+  
+  // case 3d |-> 1d array , larger bins
+  vector<int> indices3d_2  {3,1,5};
+  vector<int> binSizes3d_2 {5,3,6};
+  
+  SECTION ( " 3d |-> 1d map , larger bins" ) {
+    REQUIRE( Utility::linearizeIndices( indices3d_2 , binSizes3d_2 ) == 83 );
+  }
 
 
 }
