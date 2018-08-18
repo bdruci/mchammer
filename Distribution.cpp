1252caa9202cef03c9a072ff8fe6d1ab604990f5
Eric Pearson <peareric@umich.edu>
2018-08-18 19:31:06 -0400

    Reformatted, added delegating constructors

--- Distribution.cpp
+++ Distribution.cpp
@@ -8,184 +8,185 @@ const double pi = acos(-1.0);
 
 double uniformContinuous::sample() {
 
-	double width = end - start;
+  double width = end - start;
 
-	double inbetween = (Urand() * width) + start;
+  double inbetween = (Urand() * width) + start;
 
-	return inbetween;
+  return inbetween;
 }
 
 double exponentialContinuous::sample() {
 
-	double inbetween = (-log(Urand())/lambda) + start;
+  double inbetween = (-log(Urand())/lambda) + start;
 
-	return inbetween;
+  return inbetween;
 }
 
 double gaussian::sample() {
 
-	//Box-Muller method to get standard normally distributed point N(0,1)
-	double norm = sqrt(-2*log(Urand())) * cos(2*pi*Urand());
+  //Box-Muller method to get standard normally distributed point N(0,1)
+  double norm = sqrt(-2*log(Urand())) * cos(2*pi*Urand());
 
-	//Shift point using the location-scale property of standard normal fxns
-	norm = norm*sigma + mu;
+  //Shift point using the location-scale property of standard normal fxns
+  norm = norm*stdev + mu;
 
-	return norm;
+  return norm;
 }
 
 point isotropicDirection::sample() {
 
-	double mu = 2 * Urand() - 1;
+  double mu = 2 * Urand() - 1;
 
-	double phi = 2 * pi*Urand();
-	double omegaX=mu; 
-	double omegaY=sqrt(1-mu*mu)*cos(phi); //sin(acos(mu)) 
-	double omegaZ=sqrt(1-mu*mu)*sin(phi);
-	point dir = point(omegaX,omegaY,omegaZ);
+  double phi = 2 * pi*Urand();
+  double omegaX=mu; 
+  double omegaY=sqrt(1-mu*mu)*cos(phi); //sin(acos(mu)) 
+  double omegaZ=sqrt(1-mu*mu)*sin(phi);
+  point dir = point(omegaX,omegaY,omegaZ);
 
-	return dir;
+  return dir;
 }
 
 point cuboidGeometry::sample() {
 
-	double x, y, z;
-	x = (Urand()-0.5)*xaxis;
-	y = (Urand()-0.5)*yaxis;
-	z = (Urand()-0.5)*zaxis;
+  double x, y, z;
+  x = (Urand()-0.5)*xaxis;
+  y = (Urand()-0.5)*yaxis;
+  z = (Urand()-0.5)*zaxis;
 
-	point pos(x + x0, y + y0, z + z0);
+  point pos(x + x0, y + y0, z + z0);
 
-	return pos;
+  return pos;
 }
 
 point sphericalGeometry::sample() {
 
-	//Changed from original code
-	double x;
-	double y;
-	double z;
-	if( radInner !=0 )
-	{
-		double thickness = (radOuter - radInner) * Urand();
-		point origin(x0,y0,z0);
-		isotropicDirection unitRand;
+  double x;
+  double y;
+  double z;
+  if( radInner !=0 )
+  {
+    double thickness = (radOuter - radInner) * Urand();
+    point origin(x0,y0,z0);
+    isotropicDirection unitRand;
 
-		point unitPoint = unitRand.sample();
-		//Scale the random point to the thickness of the shell
-		unitPoint = unitPoint * thickness;
-		//Add the inner rad so point is inside of shell
-		unitPoint = unitPoint + radInner;
-		//Add the origin to place the point in the correct 3-d  space
-		unitPoint = unitPoint + origin;
+    point unitPoint = unitRand.sample();
+    //Scale the random point to the thickness of the shell
+    unitPoint = unitPoint * thickness;
+    //Add the inner rad so point is inside of shell
+    unitPoint = unitPoint + radInner;
+    //Add the origin to place the point in the correct 3-d  space
+    unitPoint = unitPoint + origin;
 
-		return unitPoint;
-	}
-	else
-	{
-		bool reject = true;
-		while(reject)
-		{
-			x = (2*Urand()-1)*radOuter;
-			y = (2*Urand()-1)*radOuter;
-			z = (2*Urand()-1)*radOuter;
-			double dist = (x*x+y*y+z*z);
-			if(dist < radOuter*radOuter)
-			{
-				reject = false;
-			}
-		}
-	}
+    return unitPoint;
+  }
+  else //Use rejection
+  {
+    bool reject = true;
+    while(reject)
+    {
+      x = (2*Urand()-1)*radOuter;
+      y = (2*Urand()-1)*radOuter;
+      z = (2*Urand()-1)*radOuter;
+      double dist = (x*x+y*y+z*z);
+      if(dist < radOuter*radOuter)
+      {
+        reject = false;
+      }
+    }
+  }
 
-	point pos(x + x0, y + y0, z + z0);
+  point pos(x + x0, y + y0, z + z0);
 
-	return pos;
+  return pos;
 }
 
 point xAnnularGeometry::sample() {
-	//I dont like rejection sampling for this becuase the inner and outer radii may be 
-	//very similar in some systems - if the radii are close and large it may take a very long
-	//time to actually guess a point in the box on the annulus
-	//However - if a cylinder is passed I do switch to rejection
 
-	double x, y, z;
+  double x, y, z;
 
-	x = height*Urand();
+  x = height*Urand();
 
-	if(radInner != 0) {
-		double phi = 2 * pi*Urand();
-		double dist = std::sqrt(radInner*radInner + (radOuter*radOuter - radInner*radInner)*Urand());	
-		y = dist*cos(phi);
-		z = dist*sin(phi);
-	}
-	else{
-		bool reject = true;
-		while(reject)
-		{
-			y = (2*Urand()-1)*radOuter;
-			z = (2*Urand()-1)*radOuter;
-			double dist = (y*y+z*z);
-			if(dist < radOuter*radOuter)
-				reject = false;
-		}
-	}
-	//switched to adding the origin
-	point pos = point(x + x0, y + y0, z + z0);
+  if(radInner != 0) 
+  {
+    double phi = 2 * pi*Urand();
+    double dist = std::sqrt(radInner*radInner + (radOuter*radOuter - radInner*radInner)*Urand()); 
+    y = dist*cos(phi);
+    z = dist*sin(phi);
+  }
+  else //Use rejection
+  {
+    bool reject = true;
+    while(reject)
+    {
+      y = (2*Urand()-1)*radOuter;
+      z = (2*Urand()-1)*radOuter;
+      double dist = (y*y+z*z);
+      if(dist < radOuter*radOuter)
+        reject = false;
+    }
+  }
+  //switched to adding the origin
+  point pos = point(x + x0, y + y0, z + z0);
 
-	return pos;
+  return pos;
 }
 
 point yAnnularGeometry::sample() {
 
-	double x, y, z;
-	y = height*Urand();
-	if(radInner != 0){
-		double phi = 2 * pi*Urand();
-		double dist = std::sqrt(radInner*radInner + (radOuter*radOuter - radInner*radInner)*Urand());
-		x = dist*cos(phi);
-		z = dist*sin(phi);
-	}
-	else{
-		bool reject = true;
-		while(reject)
-		{
-			x = (2*Urand()-1)*radOuter;
-			z = (2*Urand()-1)*radOuter;
-			double dist = (x*x+z*z);
-			if(dist < radOuter*radOuter)
-				reject = false;
-		}
-	}
-	//switched to adding the origin
-	point pos = point(x + x0, y + y0, z + z0);
+  double x, y, z;
+  y = height*Urand();
+  if(radInner != 0)
+  {
+    double phi = 2 * pi*Urand();
+    double dist = std::sqrt(radInner*radInner + (radOuter*radOuter - radInner*radInner)*Urand());
+    x = dist*cos(phi);
+    z = dist*sin(phi);
+  }
+  else //Use rejection
+  {
+    bool reject = true;
+    while(reject)
+    {
+      x = (2*Urand()-1)*radOuter;
+      z = (2*Urand()-1)*radOuter;
+      double dist = (x*x+z*z);
+      if(dist < radOuter*radOuter)
+        reject = false;
+    }
+  }
+  //switched to adding the origin
+  point pos = point(x + x0, y + y0, z + z0);
 
-	return pos;
+  return pos;
 }
 
 point zAnnularGeometry::sample() {
 
-	double x,y,z;
-	z = height*Urand();
-	if(radInner != 0){
-		double phi = 2 * pi*Urand();
-		double dist = std::sqrt(radInner*radInner + (radOuter*radOuter - radInner*radInner)*Urand());
-		x = dist*cos(phi);
-		y = dist*sin(phi);
-	}
-	else{
-		bool reject = true;
-		while(reject)
-		{
-			x = (2*Urand()-1)*radOuter; 
-			y = (2*Urand()-1)*radOuter;
-			double dist = (x*x+y*y);
-			if(dist < radOuter*radOuter)
-				reject = false;
-		}
-	}
-	//switched to adding the origin
-	point pos = point(x + x0, y + y0, z + z0);
+  double x,y,z;
+  z = height*Urand();
+  if(radInner != 0)
+  {
+    double phi = 2 * pi*Urand();
+    double dist = std::sqrt(radInner*radInner + (radOuter*radOuter - radInner*radInner)*Urand());
+    x = dist*cos(phi);
+    y = dist*sin(phi);
+  }
+  else //Use rejection
+  {
+    bool reject = true;
+    while(reject)
+    {
+      x = (2*Urand()-1)*radOuter; 
+      y = (2*Urand()-1)*radOuter;
+      double dist = (x*x+y*y);
+      if(dist < radOuter*radOuter)
+        reject = false;
+    }
+  }
+  //switched to adding the origin
+  point pos = point(x + x0, y + y0, z + z0);
 
-	return pos;
+  return pos;
 }
 
 
--- Distribution.h
+++ Distribution.h
@@ -11,180 +11,233 @@
 
 using namespace std;
 
-//TODO: Make better tests 
-//Is the current return for norm okay?
-
+//Base class with templated return type for sample
 template <class T> 
 class Distribution {
 
 private:
 
-	string name;
+  string name;
 
 public:
 
-	Distribution( string name_in ) : name(name_in) {};
-	~Distribution() {};
+  Distribution( string name_in ) : name(name_in) {};
+  ~Distribution() {};
 
-	virtual string getName() final { return name; };
+  virtual string getName() final { return name; };
 
-	virtual T sample() = 0;
+  virtual T sample() = 0;
 
 };
 
-//Returns a random point between the start and the end 
+
+//Returns a random double between the start and the end 
 class uniformContinuous : public Distribution<double> {
 
 private:
 
-	double start, end;
+  double start, end;
 
 public:
 
-	uniformContinuous( double s_in, double e_in ) : start(s_in), end(e_in), 
-					   Distribution<double>("Uniform") { assert( start < end ); };
-	~uniformContinuous() {};
+  uniformContinuous( double s_in, double e_in ) : start(s_in), end(e_in), 
+                     Distribution<double>("Uniform") { 
 
-	double sample();
+    assert( !Utility::FloatEqual(start, end, .0001) ); 
+    if(start > end)
+    {
+      start = end;
+      end = s_in;
+    }
+
+  };
+  ~uniformContinuous() {};
+
+  double sample();
+
 };
 
+
+//Returns a exponentially distributed point from start point with passed rate
 class exponentialContinuous : public Distribution<double> {
 
 private:
 
-  	double start, lambda;
+    double start, lambda;
 
 public:
   
-  	exponentialContinuous( double s_in, double l_in ) : start(s_in), lambda(l_in), 
-					       Distribution<double>("Exponential") {};
-	~exponentialContinuous() {};
+  exponentialContinuous( double s_in, double l_in ) : start(s_in), lambda(l_in), 
+                         Distribution<double>("Exponential") {};
+  ~exponentialContinuous() {};
 
-	double sample();
+  double sample();
+
 };
+
 
 //Returns a normally distributed point with the mean and standard deviation passed 
 class gaussian : public Distribution<double> {
 
 private: 
 
-	double mu, sigma;
+  double mu, stdev;
 
 public:
   
-  	gaussian( double mu_in, double sig_in ) : mu(mu_in), sigma(sig_in),
-  	                Distribution<double>("Normal") {};
-	~gaussian() {};
+  gaussian( double mu_in, double stdev_in ) : mu(mu_in), stdev(stdev_in),
+            Distribution<double>("Normal") {};
+  //Delegating constructor
+  gaussian() : gaussian(0,1) {};
+  ~gaussian() {};
 
-	double sample();
+  double sample();
+
 };
+
 
 //Returns a random direction unit vector 
 class isotropicDirection : public Distribution<point> {
 
 public:
 
-	isotropicDirection() : Distribution<point>("Direction") {};
-	~isotropicDirection() {};
+  isotropicDirection() : Distribution<point>("Direction") {};
+  ~isotropicDirection() {};
 
-	//Returns random direction unit vector
-	point sample();
+  //Returns random direction unit vector
+  point sample();
 
 };
+
 
 //returns a random point inside a box
 class cuboidGeometry : public Distribution<point> {
 
 private:
 
-	double x0, y0, z0, xaxis, yaxis, zaxis;
+  double x0, y0, z0, xaxis, yaxis, zaxis;
 
 public:
 
-	cuboidGeometry( double x_in, double y_in, double z_in, double h_in, double w_in, double l_in) 
-				  : x0(x_in), y0(y_in), z0(z_in), xaxis(h_in), yaxis(w_in), zaxis(l_in), Distribution<point>("Cuboid") {};
-	//if it's a cube
-	cuboidGeometry( double x_in, double y_in, double z_in, double side_in) 
-				  : x0(x_in), y0(y_in), z0(z_in), xaxis(side_in), yaxis(side_in), zaxis(side_in), 
-				    Distribution<point>("Cube") {};
-	~cuboidGeometry() {};
+  cuboidGeometry( double x_in, double y_in, double z_in, double h_in, double w_in, double l_in) 
+                : x0(x_in), y0(y_in), z0(z_in), xaxis(h_in), yaxis(w_in), zaxis(l_in), Distribution<point>("Cuboid") {};
+  //delegating constructors 
+  cuboidGeometry( double x_in, double y_in, double z_in, double side_in) 
+                : cuboidGeometry( x_in, y_in, z_in, side_in, side_in, side_in) {};
+  cuboidGeometry( point o, double h_in, double w_in, double l_in ) 
+                : cuboidGeometry( o.x, o.y, o.z, h_in, w_in, l_in) {};
+  cuboidGeometry( point o, double side_in) 
+                : cuboidGeometry( o.x, o.y, o.z, side_in, side_in, side_in) {};
+  ~cuboidGeometry() {};
 
-	//Returns random point inside sphere using rejection sampling
-	point sample();
+  //Returns random point inside sphere using rejection sampling
+  point sample();
 
 };
+
 
 //returns a random point inside a sphere
 class  sphericalGeometry : public Distribution<point> {
 
 private:
 
-	double x0, y0, z0, radInner, radOuter;
+  double x0, y0, z0, radInner, radOuter;
 
 public:
 
-	sphericalGeometry( double x_in, double y_in, double z_in, double radInner_in, double radOuter_in) 
-			 : Distribution<point>("Sphere"), x0(x_in), y0(y_in), z0(z_in), radInner(radInner_in), 
-			 radOuter(radOuter_in) {};
-	~sphericalGeometry() {};
+  sphericalGeometry( double x_in, double y_in, double z_in, double radInner_in, double radOuter_in) 
+                   : Distribution<point>("Sphere"), x0(x_in), y0(y_in), z0(z_in), radInner(radInner_in), 
+                     radOuter(radOuter_in) {};
+  //Delegating constructors
+  sphericalGeometry( double x_in, double y_in, double z_in, double rad_in ) 
+                    : sphericalGeometry( x_in, y_in, z_in, 0, rad_in) {};
+  sphericalGeometry( point o, double radInner_in, double radOuter_in ) 
+                    : sphericalGeometry( o.x, o.y, o.z, radInner_in, radOuter_in) {};   
+  sphericalGeometry( point o, double rad_in ) 
+                    : sphericalGeometry( o.x, o.y, o.z, 0, rad_in) {};
+  ~sphericalGeometry() {};
 
-	//Returns random point inside sphere using rejection sampling
-	point sample();
+  //Returns random point inside sphere using rejection sampling
+  point sample();
 
 };
+
 
 //returns a random point inside a cylinder centered on the x-axis
 class  xAnnularGeometry : public Distribution<point> {
 
 private:
 
-	double x0,y0,z0, height, radInner, radOuter;
+  double x0,y0,z0, height, radInner, radOuter;
 
 public:
 
-	xAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double radInner_in,
-	           double radOuter_in) : Distribution<point>("XAnnulus"), x0(x_in), y0(y_in), z0(z_in), 
-			   height(height_in), radInner(radInner_in), radOuter(radOuter_in) {};
-	~xAnnularGeometry() {};
+  xAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double radInner_in,
+                    double radOuter_in) : Distribution<point>("XAnnulus"), x0(x_in), y0(y_in), z0(z_in), 
+                    height(height_in), radInner(radInner_in), radOuter(radOuter_in) {};
+  //Delegating constructors
+  xAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double rad_in ) 
+                  : xAnnularGeometry( x_in, y_in, z_in, height_in, 0, rad_in) {};
+  xAnnularGeometry( point o, double height_in, double radInner_in, double radOuter_in ) 
+                  : xAnnularGeometry( o.x, o.y, o.z, height_in, radInner_in, radOuter_in) {};   
+  xAnnularGeometry( point o, double height_in, double rad_in ) 
+                  : xAnnularGeometry( o.x, o.y, o.z, height_in, 0, rad_in) {};
+  ~xAnnularGeometry() {};
 
-	point sample();
+  point sample();
 
 };
+
 
 //returns a random point inside a cylinder centered on the y-axis
 class  yAnnularGeometry : public Distribution<point> {
 
 private:
 
-	double x0,y0,z0, height, radInner, radOuter;
+  double x0,y0,z0, height, radInner, radOuter;
 
 public:
 
-	yAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double radInner_in,
-	           double radOuter_in) : Distribution<point>("YAnnulus"), x0(x_in), y0(y_in), z0(z_in), 
-			   height(height_in), radInner(radInner_in), radOuter(radOuter_in) {};
-	~yAnnularGeometry() {};
+  yAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double radInner_in,
+                    double radOuter_in) : Distribution<point>("YAnnulus"), x0(x_in), y0(y_in), z0(z_in), 
+                    height(height_in), radInner(radInner_in), radOuter(radOuter_in) {};
+  //Delegating constructors
+  yAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double rad_in ) 
+                  : yAnnularGeometry( x_in, y_in, z_in, height_in, 0, rad_in) {};
+  yAnnularGeometry( point o, double height_in, double radInner_in, double radOuter_in ) 
+                  : yAnnularGeometry( o.x, o.y, o.z, height_in, radInner_in, radOuter_in) {};   
+  yAnnularGeometry( point o, double height_in, double rad_in ) 
+                  : yAnnularGeometry( o.x, o.y, o.z, height_in, 0, rad_in) {};
+  ~yAnnularGeometry() {};
 
-	point sample();
+  point sample();
 
 };
+
 
 //returns a random point inside a cylinder centered on the z-axis
 class  zAnnularGeometry : public Distribution<point> {
 
 private:
 
-	double x0,y0,z0, height, radInner, radOuter;
+  double x0,y0,z0, height, radInner, radOuter;
 
 public:
 
-	zAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double radInner_in,
-	           double radOuter_in) : Distribution<point>("ZAnnulus"), x0(x_in), y0(y_in), z0(z_in), 
-			   height(height_in), radInner(radInner_in), radOuter(radOuter_in) {};
-	~zAnnularGeometry() {};
+  zAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double radInner_in,
+                    double radOuter_in) : Distribution<point>("ZAnnulus"), x0(x_in), y0(y_in), z0(z_in), 
+                    height(height_in), radInner(radInner_in), radOuter(radOuter_in) {};
+  zAnnularGeometry( double x_in, double y_in, double z_in, double height_in, double rad_in ) 
+                  : zAnnularGeometry( x_in, y_in, z_in, height_in, 0, rad_in) {};
+  zAnnularGeometry( point o, double height_in, double radInner_in, double radOuter_in ) 
+                  : zAnnularGeometry( o.x, o.y, o.z, height_in, radInner_in, radOuter_in) {};   
+  zAnnularGeometry( point o, double height_in, double rad_in ) 
+                  : zAnnularGeometry( o.x, o.y, o.z, height_in, 0, rad_in) {};
+  ~zAnnularGeometry() {};
 
-	point sample();
+  point sample();
 };
+
 
 //Returns a random element on the list of elements passed
 template <typename X>
@@ -192,62 +245,74 @@ class catagoricalWeighted : public Distribution<X> {
 
 private:
 
-	vector<X> elements;
-	vector<double> probabilities;
+  vector<X> elements;
+  vector<double> probabilities;
 
 public:
 
-	catagoricalWeighted( vector<X> elements_in, vector<double> probs_in ) 
-						   : Distribution<X>("catagorical"), elements(elements_in) {
+  catagoricalWeighted( vector<X> elements_in, vector<double> probs_in ) 
+                     : Distribution<X>("catagorical"), elements(elements_in) {
 
-		double totalProb = 0;
-		for(int j = 0; j < probs_in.size(); ++j){
-			totalProb += probs_in.at(j);
-		}
-		double probCounter = 0;
-		for(int i = 0; i < probs_in.size(); ++i) {
-			probCounter += probs_in.at(i)/totalProb;
-			probabilities.push_back(probCounter);
-		}
-		assert(probabilities.size() == elements.size());
-		if(!Utility::FloatEqual(probabilities.back(), 1.0, .001))
-		{
-			std::cout << "Improper probability vector passed - cumulative probability < 1 (tolerance .001)" << std::endl;
-			assert(false);
-		}
-		probabilities.back() = 1; //Assuming properly passed probabilities this will save rounding errors
-		
-	};
-	~catagoricalWeighted() {};
+    double totalProb = 0;
+    for(int j = 0; j < probs_in.size(); ++j)
+    {
+      totalProb += probs_in.at(j);
+    }
+    double probCounter = 0;
+    for(int i = 0; i < probs_in.size(); ++i) 
+    {
+      probCounter += probs_in.at(i)/totalProb;
+      probabilities.push_back(probCounter);
+    }
 
-	X sample() {
+    if(!(probabilities.size() == elements.size())) 
+    {
+      std::cout << "Improper usage - element and probability vectors must be the same size" << std::endl;
+      assert(false);
+    }
+    if(!Utility::FloatEqual(probabilities.back(), 1.0, .001))
+    {
+      std::cout << "Improper probability vector passed - cumulative probability < 1 (tolerance .001)" << std::endl;
+      assert(false);
+    }
 
-		double stopPoint = Urand();
-		for(int i = 0; i < elements.size(); ++i){
-			if(probabilities.at(i) > stopPoint){
-				return elements.at(i);
-			}
-		}
-		assert(false); //should never get this far
-		return elements.at(0);
-	}
+    probabilities.back() = 1; //Assuming properly passed probabilities this will save rounding errors
+    
+  };
+  ~catagoricalWeighted() {};
+
+  //Returns an elements with weighted probability
+  X sample() {
+
+    double stopPoint = Urand();
+    for(int i = 0; i < elements.size(); ++i)
+    {
+      if(probabilities.at(i) > stopPoint)
+      {
+        return elements.at(i);
+      }
+    }
+    assert(false); //should never get this far
+    return elements.at(0);
+  }
+
 };
+
 
 //Echos back passed element
 template <typename X>
 class delta : public Distribution<X> {
 private:
 
-	X echo;
+  X echo;
 
 public:
 
-	delta( X echo_in ) : Distribution<X>("delta"), echo(echo_in) {};
-	~delta() {};
+  delta( X echo_in ) : Distribution<X>("delta"), echo(echo_in) {};
+  ~delta() {};
 
-	X sample() {
-		return echo;
-	}
+  X sample() { return echo; }
+
 };
 
 #endif
