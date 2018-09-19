7086b5e4099f5fcd5c7004022832f49acec4fe8b
beykyle <beykyle@umich.edu>
2018-09-19 18:28:20 +0200

    Added printing functionality, wrapping that in BinningStructure

--- ParticleAttributeBinningStructure.cpp
+++ ParticleAttributeBinningStructure.cpp
@@ -4,40 +4,71 @@
  *
  *  This is a class for storing binning structures of arbitrary particle attributes 
  *  
- *  It wraps the class template Utility::BinningStucture and adds interface get Particle attributes and setup the 
- *  binning structure, with each derived class being specific to a particle attribute
+ *  It wraps the class template Utility::BinningStucture and adds interface get 
+ *  Particle attributes and setup the binning structure, with each derived class
+ *  being specific to a particle attribute
  *
- *  Should be constructed during input parsing for each type of particle attribute binning in the tally specification
+ *  Should be constructed during input parsing for each type of particle attribute 
+ *  binning in the tally specification
  *
  */
 
 #include "ParticleAttributeBinningStructure.h"
 
-// operator overload for ParticleAttribute for ostream
-std::ostream& operator<<(std::ostream& out, const ParticleAttribute attr){  
-  // map from ParticleAttribute to string
-  std::map < ParticleAttribute , std::string > particleAttributeToString;
-  if ( particleAttributeToString.size() == 0 ) {
-    particleAttributeToString[ParticleAttribute::group          ] = "energy group";
-    particleAttributeToString[ParticleAttribute::collisionOrder ] = "collision order";
-    particleAttributeToString[ParticleAttribute::angle          ] = "angle";
-  }
-  return out << particleAttributeToString[attr];
-};
-
 /* Integer Particle Attributes */
-
-int GroupBinningStructure::getIndex( Part_ptr p ) {
+int GroupBinningStructure::getIndex( Part_ptr p )
+{
   return(binning.getIndex( p->getGroup() ) );
 };
 
-int CollisionOrderBinningStructure::getIndex( Part_ptr p ) {
+void GroupBinningStructure::printTabularData( const vector< double > &data        , 
+                                              const vector< double > &uncertainty , 
+                                              std::ofstream &out                    ) 
+{
+  binning.printTabularData( data , uncertainty , out );
+}
+
+void GroupBinningStructure::printLowerEdge( unsigned int ind , 
+                                            std::ofstream &out ) 
+{
+  out << getStrAttribute(attr) << " = ";
+  binning.printLowerEdge( ind , out );
+}
+
+int CollisionOrderBinningStructure::getIndex( Part_ptr p ) 
+{
   return(binning.getIndex( p->getNumCollisions() ) );
 };
+    
+void CollisionOrderBinningStructure::printTabularData( const vector< double > &data        , 
+                                                       const vector< double > &uncertainty , 
+                                                       std::ofstream &out                    ) 
+{
+  binning.printTabularData( data , uncertainty , out );
+}
+
+void CollisionOrderBinningStructure::printLowerEdge( unsigned int ind , 
+                                                     std::ofstream &out ) 
+{
+  binning.printLowerEdge( ind , out );
+}
 
 /* Continous Particle Attributes */
 
-int AngleBinningStructure::getIndex( Part_ptr p ) {
+void HistogramBinningStructure::printTabularData( const vector< double > &data        , 
+                                                  const vector< double > &uncertainty , 
+                                                  std::ofstream &out                    ) 
+{
+  binning.printTabularData( data , uncertainty , out );
+}
+
+void HistogramBinningStructure::printLowerEdge( unsigned int ind , 
+                                                std::ofstream &out ) {
+  binning.printLowerEdge( ind , out );
+}
+
+int AngleBinningStructure::getIndex( Part_ptr p ) 
+{
   // calculates the cosine of the angle between dir and the direction of p, and gets the corresponding index from binningStructure
   return( binning.getIndex(  p->getDir() * dir  ) );
 };
--- ParticleAttributeBinningStructure.h
+++ ParticleAttributeBinningStructure.h
@@ -4,10 +4,12 @@
  *
  *  This is a class for storing binning structures of arbitrary particle attributes 
  *  
- *  It wraps the class template Utility::BinningStucture and adds interface get Particle attributes and setup the 
- *  binning structure, with each derived class being specific to a particle attribute
+ *  It wraps the class template BinningStucture<> and adds interface
+ *  Given particle attributes, it sets up the binning structure, 
+ *  with each derived class being specific to a particle attribute
  *
- *  Should be constructed during input parsing for each type of particle attribute binning in the tally specification
+ *  Should be constructed during input parsing for each type of particle attribute
+ *  binning in the tally specification
  *
  */
 
@@ -40,16 +42,35 @@ class ParticleAttributeBinningStructure {
     const ParticleAttribute attr;
 
   public:
-    ParticleAttributeBinningStructure(int sizein , ParticleAttribute a): size(sizein) , attr(a) {};
+    ParticleAttributeBinningStructure(int sizein , ParticleAttribute a): 
+      size(sizein) , attr(a) {};
     virtual int getIndex( Part_ptr p) = 0; 
     int getSize() { return(size); };
     
     ParticleAttribute getAttribute() { return(attr); };
+
+    inline static std::string getStrAttribute( ParticleAttribute attr ) {
+      switch( attr )  {
+        case ParticleAttribute::group:          return("group");          break;
+        case ParticleAttribute::collisionOrder: return("collisionOrder"); break;
+        case ParticleAttribute::angle:          return("angle");          break;
+      }
+    }
+
+    // prints the entire domain of bin edges alongside the data corresponding
+    // to each bin
+    virtual void printTabularData( const vector< double > &data        , 
+                                   const vector< double > &uncertainty ,
+                                   std::ofstream &out                    ) = 0;
+
+    // prints the lower bin edge given an ofstream and the index
+    virtual void printLowerEdge( unsigned int ind , std::ofstream &out ) = 0;
+
 };
 
-/* -------------------------------------------------------------------------------------------
+/* ------------------------------------------------------------------------------
  * Integer Particle Attributes 
- * ------------------------------------------------------------------------------------------- */
+ * ------------------------------------------------------------------------------ */
 
 class GroupBinningStructure : public ParticleAttributeBinningStructure {
   private:
@@ -58,18 +79,32 @@ class GroupBinningStructure : public ParticleAttributeBinningStructure {
     
   public:
     // default constructor - all groups
-    GroupBinningStructure(int numGroups): ParticleAttributeBinningStructure(numGroups , ParticleAttribute::group ) , 
-                                          binning(1 , numGroups + 1, numGroups  , false) {};
+    GroupBinningStructure(int numGroups): 
+      ParticleAttributeBinningStructure(numGroups , ParticleAttribute::group ) , 
+      binning(1 , numGroups + 1, numGroups  , false) {};
+
     // constructor  - single group
-    GroupBinningStructure(int numGroups , int group): ParticleAttributeBinningStructure(1 , ParticleAttribute::group) , 
-                                                      binning(group , group + 1, 1 , false) {};
+    GroupBinningStructure(int numGroups , int group): 
+      ParticleAttributeBinningStructure(1 , ParticleAttribute::group) , 
+      binning(group , group + 1, 1 , false) {};
+
     // constructor  - range of groups
-    GroupBinningStructure(int numGroups , int min , int max): ParticleAttributeBinningStructure(1 + max - min , ParticleAttribute::group) , 
-                                                              binning(min , max + 1, 1 + max - min , false) {};
+    GroupBinningStructure(int numGroups , int min , int max): 
+      ParticleAttributeBinningStructure(1 + max - min , ParticleAttribute::group) , 
+      binning(min , max + 1, 1 + max - min , false) {};
+
    ~GroupBinningStructure() {};
     
     int getIndex( Part_ptr p );
-    
+
+    // prints the entire domain of bin edges alongside the data corresponding
+    // to each bin
+    virtual void printTabularData( const vector< double > &data        , 
+                                   const vector< double > &uncertainty ,
+                                   std::ofstream &out                    );
+
+    // prints the lower bin edge given an ofstream and the index
+    void printLowerEdge( unsigned int ind , std::ofstream &out );
 };
  
 class CollisionOrderBinningStructure : public ParticleAttributeBinningStructure {
@@ -79,42 +114,73 @@ class CollisionOrderBinningStructure : public ParticleAttributeBinningStructure 
   public:
     // default constructor - all orders TODO needs adaptable binning structure
     // constructor - single order
-    CollisionOrderBinningStructure(int order): ParticleAttributeBinningStructure( 1 , ParticleAttribute::collisionOrder ) , 
-                                               binning(order , order + 1 , 1 , false) {};
+    CollisionOrderBinningStructure(int order): 
+      ParticleAttributeBinningStructure( 1 , ParticleAttribute::collisionOrder ) , 
+      binning(order , order + 1 , 1 , false) {};
+
     // constructor - range of orders
-    CollisionOrderBinningStructure(int min , int max): ParticleAttributeBinningStructure( 1 + max - min , ParticleAttribute::collisionOrder) , 
-                                                       binning(min , max + 1 , 1 + max - min , false) {};
+    CollisionOrderBinningStructure(int min , int max): 
+      ParticleAttributeBinningStructure( 1 + max - min , ParticleAttribute::collisionOrder) , 
+      binning(min , max + 1 , 1 + max - min , false) {};
+
    ~CollisionOrderBinningStructure() {};
     
     int getIndex( Part_ptr p );   
+ 
+    // prints the entire domain of bin edges alongside the data corresponding
+    // to each bin
+    virtual void printTabularData( const vector< double > &data        , 
+                                   const vector< double > &uncertainty ,
+                                   std::ofstream &out                    );
+
+    // prints the lower bin edge given an ofstream and the index
+    void printLowerEdge( unsigned int ind , std::ofstream &out );
 };
 
-/* -------------------------------------------------------------------------------------------
+/* ------------------------------------------------------------------------------
  * Continous Particle Attributes 
- * ------------------------------------------------------------------------------------------- */
+ * ------------------------------------------------------------------------------ */
 
-// all ParticleAttributeBinningStructures for continous attributes inherit from HistogramBinningStructure
+// all ParticleAttributeBinningStructures for continous attributes 
+// inherit from HistogramBinningStructure
 class HistogramBinningStructure : public ParticleAttributeBinningStructure {
   protected:
     BinningStructure<double> binning;
   public:
     //  1st bin: [min , min + (max - min)/size )
     // last bin: [max - (max - min)/size  , max)
-    HistogramBinningStructure(double min , double max , int size, ParticleAttribute a): ParticleAttributeBinningStructure(size , a) , 
-                                                                   binning(min , max , size , false) {};
+    
+    HistogramBinningStructure(double min         , 
+                              double max         ,  
+                              int size           , 
+                              ParticleAttribute a  ):
+      ParticleAttributeBinningStructure(size , a)     , 
+      binning(min , max , size , false)                {};
+
    ~HistogramBinningStructure() {};
     
     virtual int getIndex( Part_ptr p ) = 0;
+    
+    // prints the entire domain of bin edges alongside the data corresponding
+    // to each bin
+    virtual void printTabularData( const vector< double > &data        , 
+                                   const vector< double > &uncertainty ,
+                                   std::ofstream &out                    );
+
+    // prints the lower bin edge given an ofstream and the index
+    void printLowerEdge( unsigned int ind , std::ofstream &out );
 };
 
 class AngleBinningStructure : public HistogramBinningStructure {
-  // this class creates a HistogramBinningStructure in the cosine of the angle between a particles direction
-  // and an arbitrary unit vector 'dir'
+  // this class creates a HistogramBinningStructure in the cosine of the angle
+  // between a particles direction and an arbitrary unit vector 'dir'
+
   private:
     point dir;
   public:
     AngleBinningStructure(double min, double max , int size , point dirin): 
-                          HistogramBinningStructure(min , max , size , ParticleAttribute::angle) , dir( dirin / (dirin * dirin) ) {};
+         HistogramBinningStructure(min , max , size , ParticleAttribute::angle) , 
+         dir( dirin / (dirin * dirin) )                                          {};
 
     int getIndex( Part_ptr p );
 };
