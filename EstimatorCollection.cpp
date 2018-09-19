0246244740369f39719c8e132c137b1b806eb692
beykyle <beykyle@umich.edu>
2018-09-19 18:26:58 +0200

    Added output functionality, general organization and formatting

--- EstimatorCollection.cpp
+++ EstimatorCollection.cpp
@@ -2,29 +2,50 @@
  * Author: Kyle Beyer
  * Last Updated: 11/30/17
  *
- * This class is for grouping tallies to get an answer as a function of particle attributes
+ * This class is for grouping tallies to get an answer as a function of particle
+ * attributes
  *  e.g. energy group, collision order, angle, type, etc.
  */
 
 #include "EstimatorCollection.h"
 
+/* -----------------------------------------------------------------------------
+ *  Constructor 
+ *  
+ * ----------------------------------------------------------------------------- */
 
-EstimatorCollection::EstimatorCollection(std::map< ParticleAttribute , Bin_ptr > attributesin , EstimatorType t, unsigned long long numHistin): 
-                                          attributes(attributesin) , type(t) , numHist(numHistin)
+
+EstimatorCollection::EstimatorCollection( std::string namein , 
+                                          std::map< ParticleAttribute , 
+                                          Bin_ptr > attributesin , 
+                                          EstimatorType t, 
+                                          unsigned long long numHistin): 
+                                          attributes(attributesin) , 
+                                          type(t) , 
+                                          numHist(numHistin) , 
+                                          name(namein)
 {
-  // set default geometric divisor to 1
-  geometricDivisor = 1;
+  // set default geometric multiplier to 1
+  geometricMultiplier = 1;
 
   // default constructor calculates number of estimators required
   size = 1;
+    
+  // create a map from integer index to the particle attributes 
+  // used in this problem
+  unsigned int i = 0;
 
-  for (const auto &vals : attributes) {
+  for (const auto &attr : attributes) {
     // determine the total number of estimators
     // and the number needed for each attribute
-    size *= vals.second->getSize();
-    binSizes.push_back( vals.second->getSize() );
+    size *= attr.second->getSize();
+    binSizes.push_back( attr.second->getSize() );
+    
+    // set indexer 
+    indexer[i] = attr.first;
+    ++i;
   }
-  
+
   for(int i = 0; i < size; ++i) {
     // push back an estimator pointer for each bin in this attribute
     Estimator tally;
@@ -46,9 +67,15 @@ EstimatorCollection::EstimatorCollection(std::map< ParticleAttribute , Bin_ptr >
 };
 
 
+/* -----------------------------------------------------------------------------
+ *  EstimatorType, accessors, and helper functions
+ *  
+ * ----------------------------------------------------------------------------- */
+
 // checks if an estimator type is in 
 // the possible choices
-bool EstimatorCollection::validType( EstimatorCollection::EstimatorType t ) {
+bool EstimatorCollection::validType( EstimatorCollection::EstimatorType t ) 
+{
   bool valid = false;
   for (auto t : possibleTypes ) {
     if ( t == type) {
@@ -58,12 +85,14 @@ bool EstimatorCollection::validType( EstimatorCollection::EstimatorType t ) {
   return(valid);
 }
 
-
-EstimatorCollection::EstimatorType EstimatorCollection::getType() {
+EstimatorCollection::EstimatorType EstimatorCollection::getType() 
+{
   return(type);
 };
 
-std::string EstimatorCollection::getStrType() {
+// inline static function
+std::string EstimatorCollection::getStrType() 
+{
   switch (type) {
     case EstimatorType::SurfaceCurrent:
       return("Surface Current");
@@ -80,7 +109,32 @@ std::string EstimatorCollection::getStrType() {
   }
 }
 
-void EstimatorCollection::endHist() {
+// sets the divisor (e.g. cell or cell union volume, surface or surface union area)
+// used to normalize estimators
+void EstimatorCollection::setGeometricDivisor(double div) 
+{
+    if ( type == EstimatorType::SurfaceCurrent) {
+      // there is no geometric divisor for surface current -
+      // it is literally a count of the number of particles 
+      // that cross a surface. This should throw a runtime error.
+      std::cerr << "Error in EstimatorCollection::setGeometricDivisor"  
+                << std::endl;
+      std::cerr << "SurfaceCurrent estimators don't have geometric multipliers" 
+                << std::endl;
+      throw std::runtime_error("TypeError");
+    }
+    else {
+      geometricMultiplier = 1.0 /  div;
+    }
+}
+
+/* -----------------------------------------------------------------------------
+ *  Scoring functions
+ *  
+ * ----------------------------------------------------------------------------- */
+
+void EstimatorCollection::endHist()
+{
   for(auto estimator : estimators) {
     estimator->endHist();
   }
@@ -88,25 +142,27 @@ void EstimatorCollection::endHist() {
 
 
 // return unraveled linear index of n-dimensional histogram binning structure
-int EstimatorCollection::getLinearIndex(Part_ptr p ) {
-  vector<int> indices;
+int EstimatorCollection::getLinearIndex(Part_ptr p ) 
+{
+  vector<unsigned int> indices;
   for(auto const& attribute : attributes) {
     int index = attribute.second->getIndex(p);
     if(index >= 0) {
       // if the particle attribute is within the binning range, append the index to the n-dimensional indices
-      indices.push_back( index );
+      indices.push_back( static_cast< unsigned int > (index) );
     }
     else { 
       // if one of the particle attributes is outisde the binning range, return -1 from the function 
       return(-1);
     }
   }
-  
+
   return( Utility::linearizeIndices( indices ,  binSizes ) );
 };
 
 // score an estimator
-void EstimatorCollection::score(Part_ptr p  , double multiplier) {
+void EstimatorCollection::score(Part_ptr p  , double multiplier) 
+{
   int i =  getLinearIndex(p);
   if( i >= 0  ) { 
     // if the particle attributes are within the binning range
@@ -115,32 +171,147 @@ void EstimatorCollection::score(Part_ptr p  , double multiplier) {
   }
 }
 
-// sets the divisor (e.g. cell or cell union volume, surface or surface union area)
-// used to normalize estimators
-void EstimatorCollection::setGeometricDivisor(double div) {
-    if ( type == EstimatorType::SurfaceCurrent) {
-      // there is no geometric divisor for surface current - it is literally a count of the number of particles 
-      // that cross a surface. This should throw a runtime error.
-      std::cerr << "Error in EstimatorCollection::setGeometricDivisor"  << std::endl;
-      std::cerr << "SurfaceCurrent estimators don't have geometric multipliers" << std::endl;
-      throw std::runtime_error("TypeError");
+/* -----------------------------------------------------------------------------
+ *  Output
+ *  
+ * ----------------------------------------------------------------------------- */
+
+// Create an output file for this estimator collection
+// and call printLoop() to write all the data to it
+void EstimatorCollection::printEstimators( ) 
+{
+
+  // set output file name and open stream
+  std::string fname = "outfiles/" + name + ".out";
+  std::cout << "Writing estimator: " << name << " to " << fname << std::endl;
+  std::ofstream out;
+  out.open(fname , std::ofstream::out | std::ofstream::trunc );
+
+  if( !out.is_open() ) {
+    std::cerr << "Error opening estimator output file " << fname << std::endl;
+  }
+  else {
+
+    // print header identifying estimator
+    out << "Estimator name: " << name         << std::endl
+        << "Estimator type: " << getStrType() << std::endl << std::endl;
+
+    // initialize a vector that indexes the 1st n-1 dimensions of the histogram
+    // starts on all 0's -> 1st element of each dimension
+    std::vector< unsigned int > index(binSizes.size() - 1 , 0);
+
+    // the number of elements in the 1st N-1 dimensions of the histogram
+    unsigned int nTables = size / binSizes.back();
+
+    // iterate through the tables and print data
+    // TODO: store in tree structure for later access
+    for( int i = 0; i < nTables; ++i ) {
+      // print table number and independent variable values for 1st n-1 
+      // particle attributes
+      out << "Table " << i + 1 << ": ";
+      printTableHeader( index , out );
+      
+      // print the data table for the last dimension 
+      printTable( index , out );
+
+      advanceIndex( index );
     }
-    else {
-      geometricDivisor = div;
-    }
+  }
 }
 
+// prints the lower bin edge for the bins indexed by index 
+// in the 1st N-1 dimensions in the histogram
+void EstimatorCollection::printTableHeader( const vector< unsigned int > &index , 
+                                            std::ofstream &out                    )
+{
+  for(int i = 0; i < index.size() - 1; ++i){
+    (attributes[ indexer[ index[i] ] ])->printLowerEdge(index[i] , out);
+    out << " , ";
+  }
+  (attributes[ indexer[index.back() ] ])->printLowerEdge(index.back() , out);
+  out << std::endl;
+}
+
+// Fetches the data table for the last dimension in the histogram 
+// corresponding to the elements in the 1st n-1 dimensions
+// described by index
+//
+// Then feeds the data and uncertainties to the ParticleAttributeBinningStructure 
+// to print in a tabular form alongisde the bin edges
+void EstimatorCollection::printTable( const vector< unsigned int > &index , 
+                                      std::ofstream &out                    ) 
+{ 
+  // initialize data and variance containers
+  std::vector< double > dat(binSizes.back() , 0);
+  std::vector< double > var(binSizes.back() , 0);
+
+  // create vector for full ND index
+  vector< unsigned int > fullIndex = index;
+  fullIndex.push_back(0);
+  unsigned int linearIndex;
+
+  // populate data and uncertainty with the corresponding
+  // estimator values
+  for( int i = 0; i < binSizes.back(); ++i) {
+    fullIndex[ binSizes.back() - 1 ] = i;
+    unsigned int linearIndex = Utility::linearizeIndices( fullIndex , binSizes );
+    dat[i] = estimators[ linearIndex ]->getEstimate(numHist) * geometricMultiplier;
+    var[i] = estimators[ linearIndex ]->getUncertainty(numHist) *
+             geometricMultiplier;
+  }
+
+  // send the tabular data and output stream to the 
+  // ParticleAttributeBinningStructure in the Nth dimension of the histogram
+  // to print
+  (attributes[ indexer[ index.size() ] ])->printTabularData(dat , var , out);
+}
+
+// Description: 
+//    increments an n dimensional index one step further through the histogram
+//    space of independent variablesa
+//      e.g. in the direction from (0,0,...) --> (binSize1,binSize2,...)
+//    incrementing the nth dimension 1st, then n-1st, etc.
+// Requires: n-sized vector giving an index of an ndimensional histogram
+// Needs   : binSizes - the number of elements in each dimension
+// Modifies: the given vector, by incrementing it one step forward without
+//           overflowing the number of elements in any dimension
+//           e.g.: index=(0,2,3), binSizes=(5,5,5) --> index=(1,2,4)
+//                 index=(0,2,4), binSizes=(5,5,5) --> index=(1,3,0)
+void EstimatorCollection::advanceIndex( vector< unsigned int > &index ) 
+{
+  for( int i = index.size() - 1; i >= 0; --i) {
+    // if the index is at the maximum bin for the the attribute
+    if (index[i] == binSizes[i] - 1) {
+      // reset it to 0
+      index[i] = 0;
+    }
+    else {
+      // otherwise increment it and exit
+      index[i]++;
+      break;
+    }
+  }
+}
+
+/* -----------------------------------------------------------------------------
+ *  Testing and error checking
+ *  
+ * ----------------------------------------------------------------------------- */
+
 // generic out of range throw for checkEstimator and checkUncertainty
-void EstimatorCollection::throwOutOfRange(std::string str) {
+void EstimatorCollection::throwOutOfRange(std::string str) 
+{
     std::cerr << "Error in EstimatorCollection::" << str         << std::endl
               << "Attempted to access an out of range Estimator" << std::endl;
     throw std::runtime_error("OutOfRangeError");
 }
 
+
 // check estimator value at indices
-double EstimatorCollection::checkEstimator( vector<int> indices ) {
-  int l = Utility::linearizeIndices(indices , binSizes);
-  if ( l >= 0 and l < size ) {
+double EstimatorCollection::checkEstimator(const vector< unsigned int > &indices ) 
+{
+  unsigned int l = Utility::linearizeIndices(indices , binSizes);
+  if ( l < size ) {
     return( estimators.at(l)->getEstimate( numHist )  );
   }
   else {
@@ -149,9 +320,10 @@ double EstimatorCollection::checkEstimator( vector<int> indices ) {
 }
 
 // check estimator uncertainty at indices
-double EstimatorCollection::checkUncertainty( vector<int> indices ) {
-  int l = Utility::linearizeIndices(indices , binSizes);
-  if ( l >= 0 and l < size ) {
+double EstimatorCollection::checkUncertainty(const vector< unsigned int > &indices ) 
+{
+  unsigned int l = Utility::linearizeIndices(indices , binSizes);
+  if ( l < size ) {
     return( estimators.at(l)->getUncertainty( numHist)  );
   }
   else {
--- EstimatorCollection.h
+++ EstimatorCollection.h
@@ -2,20 +2,30 @@
  * Author: Kyle Beyer
  * Last Updated: 11/30/17
  *
- * This class is for grouping tallies to get an answer as a function of particle attributes
- *  e.g. energy group, collision order, angle, type, etc.
+ * EstimatorCollection acts as an n dimensional histogram
+ * with the space of indepenent variables being a 
+ * linear combination of particle attributes, 
+ * with the frequency of a point within that space
+ * being the score contributed by particles with that
+ * set or combination of attributes
  *
- *  An EstimatorCollection is created at input, and belongs to one or more geometric objects
- *  (e.g. cell, surface, tet). At this time, all the particlr attributes to be binned over
- *  and their respective structures are required, as well as the type of Estimator being created.
+ * This class is for grouping tallies to get an answer as a function of 
+ * particle attributes
+ *   e.g. energy group, collision order, angle, type, etc.
  *
- *  At this time, a geometric divisor can be set to normalize the results of the estimator,
- *  based on it's type and the geometric objects it belongs to.
- *  
+ * An EstimatorCollection is created at input, and belongs to one or more 
+ * geometric objects 
+ * (e.g. cell, surface, tet). 
+ * At this time, all the particle attributes to be binned over
+ * and their respective structures are required, 
+ * as well as the type of Estimator being created.
  *
- *  Scoring is handled by event handler, which determines the correct multiplier based on the 
- *  type of Estimator, and feeds the particle and multipler to the EstimatorCollection for 
- *  scoring
+ * At this time, a geometric divisor can be set to normalize the results 
+ * of the estimator, based on it's type and the geometric objects it belongs to.
+ * 
+ * Scoring is handled by event handler, which determines the correct 
+ * multiplier based on the type of Estimator, and feeds the particle 
+ * and multipler to the EstimatorCollection for scoring
  *
  *
  */
@@ -26,6 +36,7 @@
 #include <map>
 #include <memory>
 #include <stdexcept>
+#include <fstream>
 
 #include "Utility.h"
 #include "Particle.h"
@@ -40,6 +51,11 @@ typedef std::shared_ptr<ParticleAttributeBinningStructure> Bin_ptr;
 
 class EstimatorCollection {
   public:
+  /* ----------------------------------------------------------------------------
+   *  EstimatorType, accessors, and helper functions
+   *  
+   * ---------------------------------------------------------------------------- */
+   
     // Types of Estimators
     // Allows EventHandler to pass in the correct multiplier
     // corresponding to the estimator type
@@ -57,6 +73,9 @@ class EstimatorCollection {
     std::string   getStrType();
     
   private:
+    // unique name identifying estimator
+    std::string name;
+
     // syntactic sugar for iterating through valid types
     // new types added to EstimatorType must also be added
     // to possibleTypes
@@ -70,63 +89,119 @@ class EstimatorCollection {
     // this holds the type 
     EstimatorType type;
     
+    // number of source particles
+    const unsigned long long numHist;
+    
     // a multiplier to normalize an estimate by e.g. volume of a cell/cells or area
     // of a surface/surfaces
-    double geometricDivisor = 1;
+    double geometricMultiplier = 1;
     
     // number of Estimators in the collection
-    // size = sum_i N_i , where i denotes an attributed the estimator collection is binned over
+    // size = sum_i N_i , where i denotes an attributed the estimator collection is 
+    // binned over
     // and N_i is the number of bins in assigned to that attribute
     // e.g. 2 groups, and 0 through 3 collisions would be size = 2 * 4 = 8
     int size;
 
     // the number of bins over each attribute
-    vector <int>  binSizes;
+    vector <unsigned int>  binSizes;
     
     // the binning structure for each attribute
-    std::map < ParticleAttribute , Bin_ptr >  attributes;
+    std::map< ParticleAttribute , Bin_ptr >  attributes;
+  
+    // an indexer for iterating over the dimensions of the histogram 
+    // at output time
+    std::map< unsigned int , ParticleAttribute > indexer;
 
     // all the estimators in the collection
     // these are what's actually being scored
-    vector   < Estimator_ptr >  estimators;
+    vector< Estimator_ptr >  estimators;
     
     // find index of estimator to score
     // the second boolean value in the pair is true of the particle has all it's 
     // attributes within the range of binning structures for this tally
     int getLinearIndex(Part_ptr p);
 
-    // number of source particles
-    const unsigned long long numHist;
+  /* ----------------------------------------------------------------------------
+   *  Output - private
+   *  
+   * ---------------------------------------------------------------------------- */
+    
+    // prints the lower bin edge for the bins indexed by index 
+    // in the 1st N-1 dimensions in the histogram, to a given ofstream
+    void printTableHeader( const vector< unsigned int >& , std::ofstream& );
+    
+    // prints the table of estimator scores for all elements in the 
+    // last dimension of the histogram, given the index of the 
+    // 1st N-1 dimensions, to a given ofstream
+    void printTable( const vector< unsigned int >& , std::ofstream& );
+
+    // increments an n dimensional index one step further through the histogram
+    // space of independent variablesa
+    //    e.g. in the direction from (0,0,...) --> (binSize1,binSize2,...)
+    //    incrementing the nth dimension 1st, then n-1st, etc.
+    void advanceIndex( vector< unsigned int >&);
 
   public:
-    EstimatorCollection(std::map< ParticleAttribute , Bin_ptr > attributesin , EstimatorType t , unsigned long long numHistin);
+
+  /* ----------------------------------------------------------------------------
+   *  Constructor/destructor
+   *  
+   * ---------------------------------------------------------------------------- */
+
+    EstimatorCollection( std::string namein                                   , 
+                         std::map< ParticleAttribute , Bin_ptr > attributesin , 
+                         EstimatorType t                                      ,
+                         unsigned long long numHistin                           );
+
    ~EstimatorCollection() {};
+
+  /* ----------------------------------------------------------------------------
+   *  Scoring functions - public
+   *  
+   * ---------------------------------------------------------------------------- */
 
     // score a tally in the correct bin, with a given multiplier
     void score(Part_ptr p, double multiplier);
-
-    // set the geometric divisor to normalize estimators
-    // implementation depends on type
-    void setGeometricDivisor(double div);
-
-    //gets
-    int getSize() { return( size ); };
     
     // end the history for all the Estimators
     void endHist();
 
-    // print estimators
-    void printEstimators( std::string filename);
+    // set the geometric divisor to normalize estimators
+    // implementation depends on type
+    // will divide all scores by div at output time
+    void setGeometricDivisor(double div);
+  
+  /* ----------------------------------------------------------------------------
+   *  Accessors
+   *  
+   * ---------------------------------------------------------------------------- */
+
+    int getSize()         { return( size ); };
     
+    std::string getName() { return(name);    };
+    
+  /* ----------------------------------------------------------------------------
+   *  Output - public 
+   *  
+   * ---------------------------------------------------------------------------- */
+
+    // format and append independent variables vs. estimators 
+    // to an output filestream
+    void printEstimators();
+   
+  /* ----------------------------------------------------------------------------
+   *  Testing and error checking
+   *  
+   * ---------------------------------------------------------------------------- */
+
     // for checking values in a specific bin in EstimatorCollection
     // only used for testing
-    double checkEstimator(   vector< int > indices );
-    double checkUncertainty( vector< int > indices );
+    double checkEstimator(   const vector< unsigned int > &indices );
+    double checkUncertainty( const vector< unsigned int > &indices );
     
     // error handling - thrown when binningStructure returns an out of range index
     void throwOutOfRange( std::string throwLocation );
-  
 };
-
 
 #endif
